7c9e10044746fcbb9f3776f81d73682344b1d4a6
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-04-27 19:34:26 +0300

    work3

---
+++ work3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A%2 == 0)
+{
+    Console.WriteLine("чет");
+}
+else
+{
+    Console.WriteLine("нечет");
+}
