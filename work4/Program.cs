d4e5c3e0430fd57e4b005bcc1652d8cd32c19c73
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-04-27 19:34:33 +0300

    work4

---
+++ work4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= C; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    
+}
