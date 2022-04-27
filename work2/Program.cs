9c36a9d8530837dea561c8894c339bcff22717bf
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-04-27 19:34:20 +0300

    work2

---
+++ work2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if (A > B && A > C)
+{
+    Console.WriteLine("Первое число максимальное");
+}
+if (B > C && B > A)
+{
+    Console.WriteLine("Второе число максимальное");
+}
+else
+{   
+    Console.WriteLine("Третье число максимальное");
+}
