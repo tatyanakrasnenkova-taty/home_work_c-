ace7684453e6d3db9d683d34a8e0608d6803e508
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-04-27 19:34:14 +0300

    work1

---
+++ work1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A > B) 
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+{   
+    Console.WriteLine("Второе число больше первого ");
+}
